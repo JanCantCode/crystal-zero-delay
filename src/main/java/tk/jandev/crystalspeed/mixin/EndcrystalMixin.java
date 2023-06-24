afdfe8b3db0201e2f89885ad09a26914f049b7f3
SummerNugget <summernugget1@gmail.com>
2023-06-24 19:55:36 +0300

    added weakness and strength checks

--- src/main/java/tk/jandev/crystalspeed/mixin/EndcrystalMixin.java
+++ src/main/java/tk/jandev/crystalspeed/mixin/EndcrystalMixin.java
@@ -2,21 +2,34 @@ package tk.jandev.crystalspeed.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EndCrystalEntity.class)
 public abstract class EndcrystalMixin {
 
     @Shadow public abstract void kill();
+    
+    MinecraftClient mc = MinecraftClient.getInstance();
 
     @Inject(at = @At("HEAD"), method = "damage")
     private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        this.kill();
+        if (mc.player != null) {
+            StatusEffectInstance weakness = mc.player.getStatusEffect(StatusEffects.WEAKNESS);
+            StatusEffectInstance strength = mc.player.getStatusEffect(StatusEffects.STRENGTH);
+            
+            if (weakness != null && strength != null) {
+                if (strength.getAmplifier() > weakness.getAmplifier()) {
+                    this.kill();
+                }
+            }
+        }
     }
 }
